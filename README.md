4bc383488974d925c9134a9da25e4d0a46c1a511
qinggeng <qinggeng@gmail.com>
2023-03-13 17:49:06 +0800

    增加字幕信息提取函数

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 #Toolbox by chatgpt
 
 所有代码基本都是由chatgpt生成, 我只是gpt的传声筒
+
+## poetry的问题
+
+### poetry add 失败
+
+运行如下指令即可：
+```bash
+PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring poetry add <package-name>
+```
--- toolbox/subtitles/__init__.py
+++ toolbox/subtitles/__init__.py
@@ -1,3 +1,5 @@
+import pysrt
+
 def reorganize_ass_subtitle(content: str, max_len: int) -> str:
     """
     将输入的字幕文件内容重新排版后返回。
@@ -33,3 +35,39 @@ def insert_newlines(string: str, max_len: int) -> str:
     for i in range(0, len(string), max_len):
         new_string += string[i:i + max_len].strip() + '\\N'
     return new_string[:-2]  # 去掉最后的换行标志
+
+def get_srt_info(srt_string):
+    """
+    从 srt 字串中解析字幕信息
+
+    Args:
+        srt_string (str): 包含 srt 字幕的字符串
+
+    Returns:
+        list: 包含每个字幕的信息
+            字幕项包括：
+                subtitle: pysrt 字幕对象
+                duration: 以秒为单位的持续时间
+                prev_gap: 与前一项间隔时间，如果是第一个则为 None
+                next_gap: 与下一项间隔时间，如果是最后一个则为 None
+    """
+    subs = pysrt.SubRipFile.from_string(srt_string)
+
+    results = []
+    prev_end = None
+
+    for idx, sub in enumerate(subs):
+        duration = sub.end - sub.start
+        prev_gap = sub.start - prev_end if prev_end else None
+        next_gap = subs[idx + 1].start - sub.end if idx + 1 < len(subs) else None
+
+        results.append({
+            "subtitle": sub,
+            "duration": duration.total_seconds(),
+            "prev_gap": prev_gap.total_seconds() if prev_gap else None,
+            "next_gap": next_gap.total_seconds() if next_gap else None
+        })
+
+        prev_end = sub.end
+
+    return results
