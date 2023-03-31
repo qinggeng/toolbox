9f3973e47ed91b14a2220b63d303de57dcc6e00a
qinggeng <qinggeng@gmail.com>
2023-03-31 13:34:50 +0800

    增加一些ssh相关的工具

--- tests/test_shell_tools.py
+++ tests/test_shell_tools.py
@@ -1,7 +1,7 @@
+import pytest, subprocess
 from toolbox.shell import make_rsync_command
-
-import pytest
 from toolbox.shell import make_rsync_command
+from toolbox.shell import run_command
 
 def test_make_rsync_command():
   source_path = '/path/to/source'
@@ -47,3 +47,15 @@ def test_make_rsync_command():
 
   expected_cmd = "rsync -av --delete /path/to/source /path/to/dest --exclude '.git' --exclude '.svn'"
   assert make_rsync_command(source_path, dest_path, exclude_list, delete, dry_run) == expected_cmd
+
+def test_run_command_output():
+    result = list(run_command(['echo', 'hello']))
+    assert any(r[0] for r in result)
+
+def test_run_command_error():
+    assert list(run_command(['cat', 'nonexistent-file']))[-1][1] != 0
+
+def test_run_command_timeout():
+    result = list(run_command(['sleep', '2']))
+    assert result[0] == (False, None, '', '')
+    assert any(r[0] for r in result[1:])
